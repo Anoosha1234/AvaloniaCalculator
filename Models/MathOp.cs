1c7eb0b56e87a7ec2fb8d511919e30ce4dc18b95
Anoosha <71885232+Anoosha1234@users.noreply.github.com>
2021-04-08 17:55:01 +0200

    Add files via upload

---
+++ Models/MathOp.cs
@@ -0,0 +1,23 @@
+namespace SDTCalc.Models
+{
+    public enum MathOp
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+        Square,
+        Modulus,
+        SquareRoot,
+        Average,
+        Sin,
+        Cos,
+        Tan,
+        Power,
+        Log10,
+        Logn,
+        Round,
+        Result
+    }
+}
+
